2fe7172b154e84fd7ad86790933e05fb5e00ab8c
Kevin Kohler <kkohler@hitpointstudios.com>
2017-12-02 10:15:51 -0500

    First demo of possible backpack physics

---
+++ Assets/Scripts/DraggableObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DraggableObject : MonoBehaviour
+{
+    private bool mouseIsDown;
+    private Rigidbody2D rigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(mouseIsDown)
+        {
+            float x = Input.mousePosition.x;
+            float y = Input.mousePosition.y;
+            Camera c = Camera.main;
+
+            rigidBody.velocity = Vector2.zero;
+
+            Vector3 p = c.ScreenToWorldPoint(new Vector3(x, y, 10));
+
+            gameObject.transform.position = p;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log(gameObject.name + "  clicked");
+        mouseIsDown = true;
+    }
+
+    void OnMouseUp()
+    {
+        mouseIsDown = false;
+
+    }
+}
---
+++ Assets/Scripts/TempCharacterController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempCharacterController : MonoBehaviour {
+
+    Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A))
+        {
+            rigidBody.AddForce(new Vector2(-1000f, 0));
+            //transform.position = transform.position + new Vector3(-.01f,0,0);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            rigidBody.AddForce(new Vector2(1000f, 0));
+            //transform.position = transform.position + new Vector3(.01f, 0, 0);
+        }
+
+    }
+}
