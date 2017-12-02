c7fdbbc6d01d3947ef29ba5e7b8ed13c98594ebb
AndrewWilkinson88@gmail.com <AndrewWilkinson88@gmail.com>
2017-12-02 13:39:18 -0500

    In-Progress Character Movement

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public GameObject playerCharacter;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
